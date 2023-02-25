eb044ff69e915135b3353a2ff270072282e15816
garlic0x1 <garlic1@protonmail.com>
2023-02-25 16:39:26 -0600

    discord function

--- notification.js
+++ notification.js
@@ -8,6 +8,37 @@ const XSS_PAYLOAD_FIRE_EMAIL_TEMPLATE = fs.readFileSync(
 	'utf8'
 );
 
+async function send_discord_notification(xss_payload_fire_data, webhook) {
+	const notification_html_email_body = mustache.render(
+		XSS_PAYLOAD_FIRE_EMAIL_TEMPLATE,
+		xss_payload_fire_data
+	);
+
+    const fire_location = (!xss_payload_fire_data.encrypted ? xss_payload_fire_data.url : 'With An Encryption Key');
+
+	const msg = {
+		subject: `[XSS Hunter Express] XSS Payload Fired On ${fire_location}`,
+		text: "Only HTML reports are available, please use an email client which supports this.",
+		html: notification_html_email_body,
+	};
+
+    body = {
+        username: "XSS hunter",
+        body: JSON.stringify(msg),
+    };
+
+	response = await fetch(webhook, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(body)
+    })
+
+	console.debug("Message emailed with status %d", response[0].statusCode);
+	return true;
+}
+
 async function send_email_notification(xss_payload_fire_data, email) {
 	const notification_html_email_body = mustache.render(
 		XSS_PAYLOAD_FIRE_EMAIL_TEMPLATE, 
